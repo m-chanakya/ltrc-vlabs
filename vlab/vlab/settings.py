4fb5c5bc95cdc948e6f0f24e9bf02393bd4c161e
chanakya <chanakya.malireddy@gmail.com>
2015-09-25 10:03:54 +0530

    Switched to database to postgres

--- vlab/vlab/settings.py
+++ vlab/vlab/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'vlab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vlabs',
+        'USER': 'vlabs',
+        'PASSWORD': 'vlabs',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
